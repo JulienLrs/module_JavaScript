5b769f02278c694dd071726b41bc55a7841f0c0b
Julien <julien.larrouse@gmail.com>
2022-03-02 09:40:21 +0100

    update01

--- script.js
+++ script.js
@@ -68,6 +68,10 @@ document.write("<br>" + Boolean(numero)); // la valeur booléenne des autres chi
 console.log(5 > 2); // booléen qui indique le resultat de la comparaison
 console.log(10 < 3);
 
+// ====================================================== //
+// ===================== CONSTANTES ===================== //
+// ====================================================== //
+
 document.write("<h2>Les constantes</h2>");
 
 // const permet de déclarer une constante et par convention elle est écrite en MAJ
@@ -75,6 +79,10 @@ const URL = "https.//monsite.com";
 
 // URL = 'autre chose';  = ERREUR car URL est constant et ne peut être modifié
 document.write(`Mon url est ${URL}`);
+
+// ====================================================== //
+// ============== OPERATEURS ARITHMETIQUES ============== //
+// ====================================================== //
 
 document.write("<h2>Opérateurs arithmétiques</h2>");
 document.write(10 + 5 + "<br>");
@@ -88,7 +96,7 @@ document.write((10 % 5) + "<br>"); // Modulo : reste de la division entière
 document.write(10 ** 5 + "<br>"); // puissance 10²
 
 let chiffre1 = 10,
-    chiffre2 = 3;
+  chiffre2 = 3;
 let resultat = chiffre1 + chiffre2;
 document.write(`${chiffre1}+${chiffre2}=${resultat}<br>`);
 document.write(`${chiffre1 + chiffre2}<br>`);
@@ -108,17 +116,17 @@ console.log(parseFloat("25.34€"));
 document.write("<h2>Structures conditionnelles</h2>");
 
 let a = 10,
-    b = 5,
-    c = 2;
+  b = 5,
+  c = 2;
 
 // Structure if/else
 // BOOLEAN
 if (a > b) {
   // si le booléen est VRAI
-    document.write("a est supérieur à b<br>");
+  document.write("a est supérieur à b<br>");
 } else {
   // si le booléen est FAUX
-    document.write("a n'est pas suppérieur à b<br>");
+  document.write("a n'est pas suppérieur à b<br>");
 }
 
 // ====================================================== //
@@ -127,7 +135,7 @@ if (a > b) {
 
 // ET : &&
 if (a > b && b > c) {
-    document.write("OK pour les deux conditions<br>");
+  document.write("OK pour les deux conditions<br>");
 }
 /*
 FAUX && FAUX = FAUX
@@ -137,13 +145,13 @@ VRAI && VRAI = VRAI
 
 // OU : ||
 if (a == 9 || b > c) {
-    document.write("Ok pour au moins une des conditions<br>");
+  document.write("Ok pour au moins une des conditions<br>");
 }
 
 // OU exclusif (simulé)
 // (!) = NOT
 if ((a == 9 && !(b > c)) || (!(a == 9) && b > c)) {
-    document.write("Une de ces conditions seulement doit être vraie<br>");
+  document.write("Une de ces conditions seulement doit être vraie<br>");
 }
 /*
 FAUX || FAUX = FAUX
@@ -154,114 +162,131 @@ VRAI (ou exclusif) VRAI = FAUX (ou exclusif)
 
 // utilisation du != (NOT egal)
 if (a != 8) {
-    document.write("a est different de 8<br>");
+  document.write("a est different de 8<br>");
 }
 
 if (!(a == 8)) {
-    document.write("a est different de 8<br>");
+  document.write("a est different de 8<br>");
 }
 /* on utilise cette écriture quand on veut exécuter du code quand une condition est fausse */
 
-let test = a > 10; // test 
+let test = a > 10; // test
 console.log(test);
 
-if(!test){
-    document.write("c'est vrai que a n'est pas supp à 10<br>");
+if (!test) {
+  document.write("c'est vrai que a n'est pas supp à 10<br>");
 }
 
 let age = 14;
 
-if(!(age>18)){
-    document.write('Tu ne rentres pas<br>');
+if (!(age > 18)) {
+  document.write("Tu ne rentres pas<br>");
 }
 
 age += 2; // age vaut 16 > age = age +2
-console.log (age);
+console.log(age);
 
-age -= 3;  // age vaut 13 > age = age - 3
+age -= 3; // age vaut 13 > age = age - 3
 
 age += 1; // age = 14
-age++; // incrémentation de 1 
+age++; // incrémentation de 1
 age--; // décrémentation de 1
 console.log(age);
 
 // number,   string(chaine de valeur)
-let varA= 1, varB ='1'
-if(varA == varB){
-    console.log("C'est la même chose");
-
+let varA = 1,
+  varB = "1";
+if (varA == varB) {
+  console.log("C'est la même chose");
 }
 // les variables sont égales en VALEUR
 
-
 // Comparaison en VALEUR et en TYPE
-if(varA === varB){
-    /* ne s'affiche pas car ce n'est pas VRAI */
-    console.log("C'est la même chose en valeur et en type"); 
+if (varA === varB) {
+  /* ne s'affiche pas car ce n'est pas VRAI */
+  console.log("C'est la même chose en valeur et en type");
 }
-
 
 // autre exemple : 0 et false sont identiques en valeur mais pas en type
-if(0 === false){
-    /* ne s'affiche pas car 0 est un nombre et false un booléen */
-    console.log("C'est la même chose");
+if (0 === false) {
+  /* ne s'affiche pas car 0 est un nombre et false un booléen */
+  console.log("C'est la même chose");
 }
-
 
 // ====================================================== //
 // ============== Structure if/else if/else ============= //
 // ====================================================== //
 // le esl/if permet d'intercaler des conditions
-if(a == 8){
-    document.write('Cas 1 : a vaut 8<br>');
-}
-
-else if (a !=10){
-    document.write('Cas 2 : a est différent de 10<br>');
-}
-
-else{
-    document.write('Cas 3 : tout le monde a faux<br>');
+if (a == 8) {
+  document.write("Cas 1 : a vaut 8<br>");
+} else if (a != 10) {
+  document.write("Cas 2 : a est différent de 10<br>");
+} else {
+  document.write("Cas 3 : tout le monde a faux<br>");
 }
 
 // ====================================================== //
 // ================== Structure switch ================== //
-// ====================================================== // 
+// ====================================================== //
 // (fonctionne si la variable comparée est toujours la même et qu'on la compare a différentes valeurs)
-let couleur = 'jaune';
+let couleur = "jaune";
 
-switch(couleur){
-    case 'bleu':console.log('vous aimez le bleu'); break;
-    case 'rouge':console.log('vous aimez le rouge'); break;
-    case 'vert':console.log('vous aimez le vert'); break;
-    default: console.log("Vous n'aimez aucune de nos couleurs disponibles (bleu, rouge, vert)");
+switch (couleur) {
+case "bleu":
+    console.log("vous aimez le bleu");
+    break;
+case "rouge":
+    console.log("vous aimez le rouge");
+    break;
+case "vert":
+    console.log("vous aimez le vert");
+    break;
+default:
+    console.log(
+    "Vous n'aimez aucune de nos couleurs disponibles (bleu, rouge, vert)"
+    );
 } /* tres interessant si 3 cas ou plus */
 
-
 // Exercice : retranscrire ceci avec if/else if/else
-if(couleur == 'rouge'){
-    document.write('Vous aimez le jaune');
+if (couleur == "rouge") {
+document.write("Vous aimez le jaune");
+} else if (couleur == "bleu") {
+document.write("Vous aimez le jaune");
+} else if (couleur == "vert") {
+document.write("Vous aimez le jaune");
+} else {
+document.write("Vous aimez le jaune<br>");
 }
-else if(couleur == 'bleu'){
-    document.write('Vous aimez le jaune');
-}
-else if(couleur == 'vert'){
-    document.write('Vous aimez le jaune');
-}
-else{
-    document.write('Vous aimez le jaune<br> ');
-}
-
 
 /* isNaN() >>> is Not a Number */
 /* !isNaN() >>> Not is Not a Number = is a number */
 
 let nombre3 = 10;
-if(!isNaN(nombre3)){
-    document.write("C'est bien un nombre !<br>");
+if (!isNaN(nombre3)) {
+document.write("C'est bien un nombre !<br>");
 }
 
-if(typeof(nombre3) == 'number'){
-    document.write("C'est bien un nombre !<br>");
+if (typeof nombre3 == "number") {
+document.write("C'est bien un nombre !<br>");
 }
+console.log(nombre3);
 
+// ~~~~~ DRY : DON'T REPEAT YOURSELF ~~~~~ //
+
+// ====================================================== //
+// =================== FORME TERNAIRE =================== //
+// ====================================================== //
+
+let sexe = "m";
+let civilite = (sexe == "m") ? "Monsieur" : "Madame";
+// condition   ?   si vrai     sinon
+console.log("Vous êtes un homme");
+/*
+let civilite;
+if( sexe == 'm'){
+    civilite = 'Monsieur';
+}
+else{
+    civilite='Madame'
+}
+*/
